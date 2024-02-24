550a859ef22f120cf218eea8c8c808d69260b435
bishnudev1 <bishnudevkhutia20@gmail.com>
2024-02-24 20:14:44 +0530

    deleted js file

--- server.ts
+++ server.ts
@@ -1,19 +1,19 @@
 import express, {Application} from 'express';
 import http from 'http';
-import WebSocket from 'ws';
+import {WebSocketServer} from 'ws';;
 
 const app:Application = express();
 
 const server = http.createServer(app);
 
-const wss = new WebSocket.Server({ server });
+const wss = new WebSocketServer({ server });
 
 
 wss.on('connection', function connection(ws:WebSocket, req:any) {
     console.log('A new client Connected!');
 
     ws.on('message', function incoming(message:String) {
-        console.log('received: %s', message);
+        console.log('%s', message);
     });
 });
 
